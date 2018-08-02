143bafdf15a5bba94a07bbcc39987ec380d289ab
Nathan Gostelow <nathan@Nathans-MBP.lan>
2018-08-02 08:17:36 +0100

    updated app to handle input of form

--- src/component/container.jsx
+++ src/component/container.jsx
@@ -10,18 +10,27 @@ class Container extends Component {
     this.state = {
       lyrics: 'i have no lyrics right now',
       negativeWords: null,
-      positiveWords: null, 
+      positiveWords: null,
+      band: 'Wham!',
+      song: 'Careless Whisper', 
       score: 0
     };
+
+    this.handleSongChange = this.handleSongChange.bind(this);
+    this.handleBandChange = this.handleBandChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount(){
-    console.log('hello world i am here ', 'for your pleasure');
-    this.LyricCall();
+
+    this.LyricCall(this.state.band, this.state.song);
   }
 
   LyricCall(band, song) {
-      fetch('https://api.lyrics.ovh/v1/Britney Spears/Toxic')
+      if(band.length < 1){
+        return
+      }
+      fetch(`https://api.lyrics.ovh/v1/${band}/${song}`)
         .then(res => res.json())
         .then(
           (result) => {
@@ -45,12 +54,43 @@ class Container extends Component {
     console.log('result', result);
 
   }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.LyricCall(this.state.band, this.state.song);
+  }
+
+  handleBandChange(event){
+    this.setState({
+      band: event.target.value
+    });
+  }
+
+  handleSongChange(event){
+    this.setState({
+      song: event.target.value
+    });
+  }
+
   render() {
     const {lyrics, positiveWords, negativeWords, score} = this.state;
 
    
     return (
       <div className="mainContainer">
+        <div className="lyricSubmition">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Band:
+              <input type="text" value={this.state.band} onChange={this.handleBandChange} />
+            </label>
+            <label>
+              Song:
+              <input type="text" value={this.state.song} onChange={this.handleSongChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
         <div className="lyricContainer">
           <h3> Lyrics: </h3>
           <pre className="lyrics">  {lyrics} </pre>
@@ -59,7 +99,6 @@ class Container extends Component {
           <div className="sentimentContainer">
             <h3> Sentiment Analysis </h3>
             <div> SCORE: {score} </div>
-            !!TO DO!! add in positive and negative words 
           </div>
         </div>
     );
